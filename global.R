306abcf47d7739c8f9613c11da7da2e323a896cb
ajayasuri <83713043+ajayasuri@users.noreply.github.com>
2021-06-08 08:19:00 -0400

    Add files via upload

---
+++ global.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(dplyr)
+
+library(DT)
+library(shiny)
+library(lubridate)
+library(naniar)
+library(plotly)
+library(reshape2)
+library(ggrepel)
+library(tokenizers)
+library(wordcloud)
+
+df <- read.csv('netflix_titles.csv', header = TRUE)
+df <- data.frame(df)
+
+# Clean data for analysis
+
+df$date_added <- as.Date(df$date_added, format = "%B %d, %Y")
+
+
+df$director[df$director == ""] <- NA
+df$cast[df$cast == ""] <- NA
+df$country[df$country == ""] <- NA
+df$rating[df$rating == ""] <- NA
+
+df <- na.omit(df)
+
